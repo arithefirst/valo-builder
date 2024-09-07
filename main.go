01a58f8be09a8029e989c705ba7d60783a664235
April Hall <ari@arithefirst.com>
2024-09-07 18:00:53 -0400

    Itterate through every weapon instead of just the one at index 0

--- main.go
+++ main.go
@@ -25,6 +25,41 @@ func getJson() []byte {
 	return body
 }
 
+func printSkins(response jsonData, index int) {
+	// Print the weapon name
+	fmt.Printf("%v Skins:\n", response.Data[index].Name)
+
+	// Loop over the skins in the weapon struct
+	for i := 0; i < len(response.Data[index].Skins); {
+		// Print skin name
+		fmt.Printf("%v\n", response.Data[index].Skins[i].Name)
+		// If the chromas != 1, loop through each variant
+		if len(response.Data[index].Skins[i].Chromas) != 1 {
+			for o := 0; o != len(response.Data[index].Skins[i].Chromas); {
+				var variantName string
+
+				// Make sure battle pass skins or 5-Tier skins cant escape the
+				// removal of the skin title from the chroma names
+				if strings.Contains(response.Data[index].Skins[i].Chromas[o].Name, "Level 4") {
+					variantName = strings.ReplaceAll(response.Data[index].Skins[i].Chromas[o].Name, fmt.Sprintf("%v Level 4\n", response.Data[index].Skins[i].Name), "")
+				} else if strings.Contains(response.Data[index].Skins[i].Chromas[o].Name, "Level 5") {
+					variantName = strings.ReplaceAll(response.Data[index].Skins[i].Chromas[o].Name, fmt.Sprintf("%v Level 5\n", response.Data[index].Skins[i].Name), "")
+				} else {
+					variantName = strings.ReplaceAll(response.Data[index].Skins[i].Chromas[o].Name, fmt.Sprintf("%v\n", response.Data[index].Skins[i].Name), "")
+				}
+
+				if strings.ToLower(response.Data[index].Skins[i].Chromas[o].Name) == strings.ToLower(response.Data[index].Skins[i].Name) {
+					fmt.Printf("  ╰─(Variant 0 Default): %v\n", response.Data[index].Skins[i].Chromas[o].Swatch)
+				} else {
+					fmt.Printf("  ╰─%v: %v\n", variantName, response.Data[index].Skins[i].Chromas[o].Swatch)
+				}
+				o += 1
+			}
+		}
+		i += 1
+	}
+}
+
 func main() {
 	var response jsonData
 	err := json.Unmarshal(getJson(), &response)
@@ -33,40 +68,9 @@ func main() {
 		return
 	}
 
-	// Print the weapon name
-	fmt.Printf("%v Skins:\n", response.Data[0].Name)
-
-	// Loop over the skins in the weapon struct
-	for i := 0; i < len(response.Data[0].Skins); {
-		// Print skin name
-		fmt.Printf("%v\n", response.Data[0].Skins[i].Name)
-		// If the chromas != 1, loop through each variant
-		if len(response.Data[0].Skins[i].Chromas) != 1 {
-			for o := 0; o != len(response.Data[0].Skins[i].Chromas); {
-				var variantName string
-
-				// Make sure battle pass skins or 5-Tier skins cant escape the
-				// removal of the skin title from the chroma names
-				if strings.Contains(response.Data[0].Skins[i].Chromas[o].Name, "Level 4") {
-					variantName = strings.ReplaceAll(response.Data[0].Skins[i].Chromas[o].Name, fmt.Sprintf("%v Level 4\n", response.Data[0].Skins[i].Name), "")
-				} else if strings.Contains(response.Data[0].Skins[i].Chromas[o].Name, "Level 5") {
-					variantName = strings.ReplaceAll(response.Data[0].Skins[i].Chromas[o].Name, fmt.Sprintf("%v Level 5\n", response.Data[0].Skins[i].Name), "")
-				} else {
-					variantName = strings.ReplaceAll(response.Data[0].Skins[i].Chromas[o].Name, fmt.Sprintf("%v\n", response.Data[0].Skins[i].Name), "")
-				}
-
-				if strings.ToLower(response.Data[0].Skins[i].Chromas[o].Name) == strings.ToLower(response.Data[0].Skins[i].Name) {
-					fmt.Printf("  ╰─(Variant 0 Default): %v\n", response.Data[0].Skins[i].Chromas[o].Swatch)
-				} else {
-					fmt.Printf("  ╰─%v: %v\n", variantName, response.Data[0].Skins[i].Chromas[o].Swatch)
-				}
-				o += 1
-			}
-		}
+	for i := 0; i < 19; {
+		printSkins(response, i)
 		i += 1
 	}
-
-	var odin weapon = response.Data[0]
-	var ares weapon = response.Data[1]
 
 }
