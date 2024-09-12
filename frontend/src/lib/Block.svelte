c2d454bbf0d086858da5cc2e3538d567fd65eda1
April Hall <ari@arithefirst.com>
2024-09-12 01:11:32 -0400

    Create the start of the frontend

---
+++ frontend/src/lib/Block.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Skin from './Skin.svelte'
+    export let weapon;
+    let posts;
+
+    async function fetchData() {
+        const response = await fetch(`http://127.0.0.1:8080/api/v1/skin/${weapon}`);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+
+    console.log(posts);
+
+</script>
+
+<main>
+    {#await fetchData() then data}
+        {#each data as skin}
+            <div>
+                <Skin src={skin.fullRender} title={skin.displayName}></Skin>
+            </div>
+        {/each}
+    {/await}
+</main>
---
+++ frontend/src/lib/Skin.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let src;
+    export let title;
+</script>
+
+<div>
+    <p><strong>{title}</strong></p>
+    <img {src} alt="{title}">
+</div>
+
+<style>
+
+    p {
+        color: #cdd6f4;
+    }
+
+    div {
+        border: 3px solid #6c7086;
+        width: 250px;
+        background-color: #1e1e2e;
+        text-align: center;
+        margin: 10px 5px;
+        font-size: 0.8rem;
+        border-radius: 15px;
+        display: inline-grid;
+    }
+
+    img {
+        width: 90%;
+        padding: 10px;
+    }
+
+</style>
