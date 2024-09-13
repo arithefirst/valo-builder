c8568bdf422e675f17e14e29ea4eda59f9ec6f92
April Hall <ari@arithefirst.com>
2024-09-13 03:02:19 -0400

    Add collection layout

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,47 @@
 <script>
   import Block from './lib/Block.svelte';
+  import Item from './lib/Item.svelte';
+
+  const defaults = new Map()
+  defaults.set("phantom", "")
+
 </script>
 
-<Block weapon="phantom"></Block>
+<div class="outer">
+    <div class="sidearms">
+        <Item weapon="classic" url="blank"/>
+        <Item weapon="shorty" url="blank"/>
+        <Item weapon="frenzy" url="blank"/>
+        <Item weapon="ghost" url="blank"/>
+        <Item weapon="sheriff" url="blank"/>
+    </div>
+
+    <div class="smgs-shotguns">
+        <Item weapon="stinger" url="blank"/>
+        <Item weapon="spectre" url="blank"/>
+        <p/>
+        <Item weapon="bucky" url="blank"/>
+        <Item weapon="judge" url="blank"/>
+    </div>
+
+    <div class="rifles-melee">
+        <Item weapon="bulldog" url="blank"/>
+        <Item weapon="guardian" url="blank"/>
+        <Item weapon="phantom" url="blank"/>
+        <Item weapon="vandal" url="blank"/>
+        <p/>
+        <Item weapon="melee" url="blank"/>
+    </div>
+
+    <div class="snipers-lmgs">
+        <Item weapon="marshal" url="blank"/>
+        <Item weapon="outlaw" url="blank"/>
+        <Item weapon="operator" url="blank"/>
+        <p/>
+        <Item weapon="ares" url="blank"/>
+        <Item weapon="odin" url="blank"/>
+    </div>
+</div>
 <footer>RIOT GAMES, VALORANT and any associated logos are trademarks, service marks, and/or registered trademarks of <a href="https://riotgames.com">Riot Games, Inc.</a></footer>
 
 <style>
@@ -18,6 +57,24 @@
     a {
         color: inherit;
         text-decoration: underline;
+    }
+
+    .outer{display: flex;}
+
+    .sidearms {
+        margin-top: 50px;
+        margin-left: 80px;
+        width: 300px;
+    }
+
+    .smgs-shotguns, .rifles-melee, .snipers-lmgs{
+        margin-top: 50px;
+        margin-left: 40px;
+        width: 300px;
+    }
+
+    p {
+        margin-bottom: 68px
     }
 
     :root {
---
+++ frontend/src/lib/Item.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let weapon
+    export let url
+</script>
+
+<svg id="" width="300px" viewBox="0 0 263 109" xmlns="http://www.w3.org/2000/svg">
+    <rect width="263" height="109" fill="#245269" />
+    <path d="M0,0 L0,109 L263,109 L263,0 M1,1 L1,108 L262,108 L262,1" fill="#386b8b" fill-rule="evenodd"/>
+    <path d="M1,1 L1,28 L28,1" fill="#3b5e76"/>
+    <text class="text" x="3" y="104" font-size="11">{weapon.toUpperCase()}</text>
+    <style>
+        @font-face {
+            font-family: tungsten;
+            src: url(../assets/Tungsten-Book.ttf);
+        }
+
+        .text {
+            font-family: 'tungsten';
+        }
+    </style>
+</svg>
+
+<style>
+    svg {
+        margin-bottom: 15px;
+    }
+</style>
