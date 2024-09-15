bb035d85832034fe0d7de9c70fc14b1353a55071
April Hall <ari@arithefirst.com>
2024-09-14 21:04:41 -0400

    Removed unnessecary variable "data"

--- frontend/src/lib/Block.svelte
+++ frontend/src/lib/Block.svelte
@@ -4,9 +4,7 @@
 
     async function fetchData() {
         const response = await fetch(`http://127.0.0.1:8080/api/v1/skin/${weapon}`);
-        const data = await response.json();
-        console.log(data);
-        return data;
+        return await response.json();
     }
 </script>
 
