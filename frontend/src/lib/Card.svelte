a1490ee899aa8e95d7c1c3f1598294951b2286c3
April Hall <ari@arithefirst.com>
2024-09-20 13:30:56 -0400

    Create Card.svelte and CardBlock.svelte

---
+++ frontend/src/lib/Card.svelte
@@ -0,0 +1,52 @@
+<script>
+    export let src;
+    export let alt
+    export let i;
+
+    import {card} from './stores.js'
+
+    function load() {
+        // Lookup the currently set weapon in the weapons table;
+        // Then set that weapon's store's value to that of src
+        card.set( {src: src, name: alt} )
+    }
+</script>
+
+<div
+        class="skinCon"
+        on:click={load}
+        on:keyup={load}
+        role="button"
+        tabindex={i}
+>
+    <p><strong>{alt}</strong></p>
+    <img {src} {alt}>
+</div>
+
+<style>
+
+    p {
+        color: #cdd6f4;
+    }
+
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        border: 3px solid #6c7086;
+        width: 180px;
+        background-color: #1e1e2e;
+        text-align: center;
+        font-size: 0.8rem;
+        border-radius: 15px;
+        margin: 10px 5px;
+        cursor: pointer;
+    }
+
+    img {
+        width: 90%;
+        padding: 10px;
+    }
+
+</style>
---
+++ frontend/src/lib/CardBlock.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+    import Card from './Card.svelte'
+    import {card} from './stores.js'
+    export let visible: boolean;
+
+    // Subscribe to the store for the current weapon
+    let alt: string
+
+    card.subscribe((val) => {
+        alt = val.name
+    })
+
+
+    let resp = fetchData();
+    let query: string;
+
+    // Function for handling searches
+    async function search() {
+        if (query === "") {
+            // If the query is empty, give the default data instead
+            console.log("Search empty. Re-setting to default.")
+            resp = fetchData();
+        } else {
+            const response = await fetch(`http://127.0.0.1:8080/api/v1/search?q=${query}&weapon=19`)
+            resp = await response.json()
+        }
+    }
+
+    async function fetchData() {
+        const response = await fetch(`http://127.0.0.1:8080/api/v1/cards`);
+        return await response.json();
+    }
+
+    function toggleVis() {
+        visible = false;
+    }
+
+</script>
+
+<div class="blur"/>
+<div class="skins-selector">
+    <svg xmlns="http://www.w3.org/2000/svg" class="close" viewBox="0 0 24 24"
+         on:click={toggleVis}
+         on:keypress={toggleVis}
+         role="button"
+         tabindex=0>
+        <!-- Close Icon -->
+        <path fill="#6c7086" d="
+         M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59
+         L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12
+         L5.7 16.89a.996.996 0 1 0 1.41 1.41
+         L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41
+         L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4"/>
+    </svg>
+    <h2>Cards</h2>
+    <div class="levels-img-chroma"></div>
+    <hr>
+    <div class="scrollable">
+        <div class="grid">
+            {#await resp then data}
+                {#if data !== null}
+                    {#each data as card, i}
+                        <!-- Card component requires i var for tabindex -->
+                        <Card {i} alt={card.displayName} src={card.largeArt}></Card>
+                    {/each}
+                {/if}
+            {/await}
+        </div>
+    </div>
+</div>
+
+<style>
+
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        padding-left: 10px;
+    }
+
+    .blur {
+        z-index: 20;
+        position: fixed;
+        content: '';
+        backdrop-filter: blur(15px);
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+    }
+
+    .skins-selector {
+        border: 3px solid #6c7086;
+        background-color: #1e1e2e;
+        border-radius: 15px;
+        width: 800px;
+        height: calc(100vh - 26px);
+        text-align: center;
+        z-index: 1000;
+        position: absolute;
+        margin: 10px 50% 10px;
+        translate: -50%;
+        overflow: hidden;
+    }
+
+    .scrollable {
+        height: calc(100% - 300px);
+        position: absolute;
+        overflow-y: scroll;
+    }
+
+    .close {
+        position: fixed;
+        right: 10px;
+        top: 10px;
+        width: 4%;
+        cursor: pointer;
+        overflow: hidden;
+    }
+
+    .levels-img-chroma {
+        position: relative;
+        height: 200px;
+    }
+
+    hr {
+        width: 90%;
+        height: 2px;
+        border-color: #6c7086;
+        background-color: #6c7086;
+    }
+
+</style>
